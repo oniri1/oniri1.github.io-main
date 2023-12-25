4424fda7f65b91811656b8e4efda07911f4ffa6c
oniri1 <oniri1@naver.com>
2023-12-25 22:24:03 +0900

    js-data

--- src/projects.js
+++ src/projects.js
@@ -3,11 +3,25 @@
 // 프로젝트 필터링 관련 로직 처리
 
 const categories = document.querySelector('.categories');
+const projects = document.querySelectorAll('.project')
 
 categories.addEventListener('click', (event) => {
     const filter = event.target.dataset.category;
 
-    console.log(filter);
+    if(filter == null){
+        return;
+    }
+
+    projects.forEach(project => {
+        console.log(project.dataset.type);
+        if(filter === 'all' || filter === project.dataset.type){
+            project.style.display = 'block';
+        } 
+        else{
+            project.style.display = 'none';
+        }
+    })
+
 
 
 })
