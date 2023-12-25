edbc6f8b0af1d87a9ee023c0dcef17b6880a414a
oniri1 <oniri1@naver.com>
2023-12-26 00:01:36 +0900

    data-category

--- src/projects.js
+++ src/projects.js
@@ -4,6 +4,7 @@
 
 const categories = document.querySelector('.categories');
 const projects = document.querySelectorAll('.project')
+const projectscontainer = document.querySelector('.projects')
 
 categories.addEventListener('click', (event) => {
     const filter = event.target.dataset.category;
@@ -11,17 +12,33 @@ categories.addEventListener('click', (event) => {
     if(filter == null){
         return;
     }
-
-    projects.forEach(project => {
-        console.log(project.dataset.type);
-        if(filter === 'all' || filter === project.dataset.type){
-            project.style.display = 'block';
-        } 
-        else{
-            project.style.display = 'none';
-        }
-    })
+    handleActiveSelection(event.target);
+    filterProjects(filter);
 
 
 
-})
+});
+
+function handleActiveSelection(target){
+        const active = document.querySelector('.category--selected');
+        active.classList.remove('category--selected');
+        target.classList.add('category--selected');
+}
+
+function filterProjects(filter){
+        // 프로젝트 필터링
+        projectscontainer.classList.add('anim-out');
+
+        projects.forEach(project => {
+            console.log(project.dataset.type);
+            if(filter === 'all' || filter === project.dataset.type){
+                project.style.display = 'block';
+            } 
+            else{
+                project.style.display = 'none';
+            }
+        });
+
+        setTimeout(()=>{
+            projectscontainer.classList.remove('anim-out');},250);
+}
