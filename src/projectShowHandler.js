fd949ef7bc6f7aca0ee3cb82fe7bddd1693bf29d
oniri1 <oniri1@naver.com>
2024-10-10 15:05:47 +0900

    feat : skillDetails Update

--- src/projectShowHandler.js
+++ src/projectShowHandler.js
@@ -1,46 +1,45 @@
 const chatSiteValues = {
   skillsTitle: "Colosseum",
   skillsDetail:
-    "팀장으로서 FrontEnd와 BackEnd를 담당하며 Socket.io, MongoDB를 R&D하여 실시간 통신을 구현하였습니다.",
+    "팀장으로서 프로젝트 전체를 담당하며 Socket.io, MongoDB를 R&D하여 실시간 통신을 구현하였습니다. <br/>프론트에서는 JavaScript를 작성하여 서버와 Axios로 통신해 데이터를 출력하였으며, 데이터베이스의 트래픽으로 활발한 채팅, 댓글을 노출시키는 기능을 구현하였습니다.",
   skillsImg: `<a href="https://main.oniri1.com/chatSite/" target="_blank"><img src="./images/projects/chatSite.jpg" style="width: 100%; height: 100%;" alt="project1"></a>`,
-  skillsLibrary: `<img src="./images/Js.png" alt="JavaScript"class="mainStack_logo">`,
-  skillsLibraryJob: `<li>CSR</li>`,
-  skillsFrameWork: `<li>cookie-parser</li><li>sequelize</li><li>mongodb</li><li>multer</li>`,
-  skillsFrameWorkJob: `<li>서버 전체 구현</li>`,
+  skillsLibrary: `<li>Axios를 사용하여 서버-클라이언트 데이터 통신</li><li>DOM 메서드를 사용한 CSR</li><li>Express router 기능을 사용하여 서버 제작</li><li>Multer로 이미지 업로드 구현</li><li>MongoDB, sequelize 라이브러리 사용 경험</li><li>socket.io를 사용 실시간 통신 구현 경험</li><li>socket.io의 room 기능 사용 경험</li>`,
+
   skillsHref:
     "https://drive.google.com/drive/folders/1gye06OWaA1VnIj6k7zW0eCBuw98OzT5V",
   skillLogos: `<img src="./images/HTML.png" alt="HTML" class="mainStack_logo"><img src="./images/cssLogo.png" alt="css"class="mainStack_logo"><img src="./images/Js.png" alt="JavaScript"class="mainStack_logo"><img src="./images/express.png" alt="express"class="mainStack_logo"><img src="./images/socket.png" alt="socket.io"class="mainStack_logo"><img src="./images/Axios.png" alt="axios"class="mainStack_logo"><img src="./images/mySQL.png" alt="mySQL"class="mainStack_logo"><img src="./images/mongoDB.png" alt="mongoDB"class="mainStack_logo">`,
+  skillFelt:
+    "팀장으로서 백과 프론트를 담당하며 실시간 채팅 사이트를 구현하게 되었습니다.<br/>진행사항 체크 및 코드 병합의 수월한 진행을 위해 팀원간의 소통과 사전 계획의 중요성을 깨닫게 되었습니다.<br/>socket.io를 구현하며 개발에 대한 방향성을 잡았으며, Axios로 통신하는 과정에서 서버-클라이언트 모델에 대한 이해도가 향상 되었습니다.<br/>또한 MongoDB와 Socket.io를 R&D 한 경험이 저의 개발자로서의 역량을 향상시켜 주었습니다.",
 };
 //
 
 const shopSiteValues = {
   skillsTitle: "Hamster_Market",
   skillsDetail:
-    "프론트엔드와 글쓰기 페이지와 유저 페이지를 중점으로 구현하고 NaverMap,OAuth를 R&D 하여 프로젝트에 적용 시켰습니다.",
+    "중고마켓 사이트를 목표로 사용자 페이지, 관리자 페이지, 배송기사 페이지를 만들어 실제 운영을 중점으로 제작하였습니다. <br/>TypeScript, React, tailwindCss를 기반으로 프론트를 구현하였으며 OAuth2.0, NaverMap 같은 OpenAPI를 R&D하여 프로젝트에 적용 시켰습니다.",
   skillsImg: `<a href="https://main.oniri1.com/twoProject/front" target="_blank"><img src="./images/projects/HamsterMarket.png" style="width: 100%; height: 100%;" alt="project2"></a>`,
-  skillsLibrary: `<li>react-query</li><li>react-router-dom</li><li>recoil</li><li>Observer</li>`,
-  skillsLibraryJob: `<li>유저 페이지 구현</li><li>게시글 쓰기 페이지 구현</li><li>전체적인 데이터 처리</li><li>React Warning 수정</li><li>NaverMap 구현</li>`,
-  skillsFrameWork: `<img src="./images/express.png" alt="express"class="mainStack_logo">`,
-  skillsFrameWorkJob: `<li>OAuth 코드 구현</li>`,
+  skillsLibrary: `<li>NaverMap API를 사용한 맵 기능</li><li>OAuth2.0으로 사용자 인증 및 권한 부여 경험</li><li>내장 API(navigator)를 사용한 GPS 기능</li><li>데이터 처리 메서드(Blob)을 활용한 이미지 데이터 핸들링 경험</li><li>React-router-dom을 활용한 SPA 제작</li><li>React-query를 사용하여 state 상태 관리</li><li>recoil로 전역 state 관리</li><li>React로 유저,글 작성 페이지 제작</li><li>정규 표현식과 메서드를 사용한 회원가입, 로그인 입력 처리</li>`,
+
   skillsHref:
     "https://drive.google.com/drive/folders/1yF5krXC3MG6jJQ8BB1w6SqyAyIUU7y94",
   skillLogos: `<img src="./images/react.png" alt="react" class="mainStack_logo"><img src="./images/tailwindcss.png" alt="tailwindcss"class="mainStack_logo"><img src="./images/express.png" alt="express"class="mainStack_logo"><img src="./images/TS.png" alt="TypeScript"class="mainStack_logo"><img src="./images/oAuth.png" alt="OAuth"class="mainStack_logo"><img src="./images/naverMap.png" alt="naverMap"class="mainStack_logo"><img src="./images/Axios.png" alt="axios"class="mainStack_logo"><img src="./images/mySQL.png" alt="mySQL"class="mainStack_logo"><img src="./images/mongoDB.png" alt="mongoDB"class="mainStack_logo">`,
+  skillFelt:
+    "팀원으로서 프론트를 담당하며 OpenAPI를 중점으로 여러 기능을 개발하게 되었습니다.<br/>React를 기반한 프로젝트 협업을 진행함으로서 코드 리뷰 및 모듈화를 통한 가독성 향상과 단일 책임 원칙의 중요성을 깨닫게 되었습니다.<br/>특히, 코드 리뷰 없이 진행한 프로젝트는 팀원의 잘못된 이해를 동반한 코드의 방향성으로 인해 많은 문제가 생길 수 있음을 알 수 있었고, 모듈화와 가독성이 부족하여 많은 고생을 하였지만 TypeScript의 타입 추론과 타입 체크 덕분에 수정을 수월하게 진행할 수 있었던 경험이 깊이 남게 되었습니다.<br/>이 경험을 통해 React 라이프 사이클 및 최적화에 대한 이해도가 향상 되었으며, 주 역할로 맡았던 페이지 기능 구현과 API를 활용한 개발을 성공적으로 완수하며 한층 더 나은 개발자로서 성장하게 되었습니다.",
 };
 
 //
 const openAiValues = {
   skillsTitle: "AI_InterViewSite",
   skillsDetail:
-    "Next.js와 Nest.js, OpenAI를 R&D하여 사용자 인터뷰 및 답변에 대한 피드백을 받을 수 있는 AI 서비스 프로젝트를 만들었습니다.",
+    "OpenAI API를 적용하여 면접관 모델과 인터뷰를 통한 피드백을 받을 수 있는 사이트를 제작하였습니다. <br/>Next.js, Nest.js, OpenAI를 사용해 서비스를 구현하고 JWT,Hash로 이용자의 로그인과 데이터를 관리하며 AWS,Git Action으로 CICD를 구성하여 배포하였습니다.",
   skillsImg: `<a href="https://main.oniri1.com/openAiProject/" target="_blank"><img src="./images/projects/openAI.png" style="width: 100%; height: 100%;" alt="project3"></a>`,
-  skillsLibrary: `<img src="./images/Next.png" alt="next" class="mainStack_logo">`,
-  skillsLibraryJob: `<img src="./images/Next.png" alt="next" class="mainStack_logo">`,
-  skillsFrameWork: `<li>typeorm</li><li>mongoose</li><li>jsonwebtoken</li><li>session-file-store</li><li>crypto</li>`,
-  skillsFrameWorkJob: `<li>openAI Fine tuning</li>`,
+  skillsLibrary: `<li>next.js 앱 라우터 방식으로 제작</li><li>tailwindCss 커스텀 애니메이션 사용 경험</li><li>JWT를 통한 유저 검증</li><li>OpenAI 서비스를 사용한 면접관 모델 생성</li><li>OpenAI API를 사용한 면접 기능</li><li>typeORM, Nest/Mongoose으로 Nest와 Database 커넥션 설정 및 데이터 관리 경험</li><li>Github Actions을 통한 CICD 구현</li>`,
+
   skillsHref:
     "https://drive.google.com/drive/folders/1rFDuQeqd_2wfpaOXx_e4-SdKtgtldlu2",
-
   skillLogos: `<img src="./images/Next.png" alt="next" class="mainStack_logo"><img src="./images/Nest.png" alt="nest"class="mainStack_logo"><img src="./images/tailwindcss.png" alt="tailwindcss"class="mainStack_logo"><img src="./images/TS.png" alt="TypeScript"class="mainStack_logo"><img src="./images/AI.png" alt="openAI"class="mainStack_logo"><img src="./images/Axios.png" alt="axios"class="mainStack_logo"><img src="./images/mySQL.png" alt="mySQL"class="mainStack_logo"><img src="./images/mongoDB.png" alt="mongoDB"class="mainStack_logo">`,
+  skillFelt:
+    "풀스택 개발 경험으로 각 기술의 개발 능력과, 인증 및 데이터 보안의 중요성과, 새로운 프레임 워크와 Open API를 공부하여 구현함으로서, 코드 분석 및 문제 해결 능력이 향상 되었습니다.<br/>또한, 말로만 듣던 AI를 백엔드에서 직접 다루어보며 기술의 발전을 실감하게 되었습니다. 이 기술을 어떻게 사용하여 더 나은 서비스나 원하는 출력을 뽑아낼 수 있는지, 다양한 요구를 어떻게 처리할 수 있는지 등등 저에게 많은 가능성을 열어주는 프로젝트로서 인상 깊이 남게 되었습니다.",
 };
 
 const skillChanger = ({
@@ -48,34 +47,30 @@ const skillChanger = ({
   skillsDetail,
   skillsImg,
   skillsLibrary,
-  skillsLibraryJob,
-  skillsFrameWork,
-  skillsFrameWorkJob,
+
   skillsHref,
   skillLogos,
+  skillFelt,
 }) => {
   const title = document.getElementById("skillsTitle");
   const detail = document.getElementById("skillsDetail");
   const img = document.getElementById("skillsImg");
 
   const library = document.getElementById("addLibFront");
-  const libWork = document.getElementById("myJobFront");
-
-  const framework = document.getElementById("addLibBack");
-  const fraWork = document.getElementById("myJobBack");
 
   const link = document.getElementById("skillsHref");
   const logos = document.getElementById("skillsLogosC");
+
+  const felt = document.getElementById("felt");
 
   title.innerText = skillsTitle;
   detail.innerHTML = skillsDetail;
   img.innerHTML = skillsImg;
   library.innerHTML = skillsLibrary;
-  libWork.innerHTML = skillsLibraryJob;
-  framework.innerHTML = skillsFrameWork;
-  fraWork.innerHTML = skillsFrameWorkJob;
+
   logos.innerHTML = skillLogos;
-  link.setAttribute("href", skillsHref); // 새로운 href 설정
+  link.setAttribute("href", skillsHref);
+  felt.innerHTML = skillFelt;
 };
 
 const projectAll = document.getElementById("projectClick");
