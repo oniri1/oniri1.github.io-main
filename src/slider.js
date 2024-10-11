a267b69c495b1d38e7c404153f3f7d016df0e39a
oniri1 <oniri1@naver.com>
2024-10-11 14:03:36 +0900

    feat : skills Btn showHandler

--- src/slider.js
+++ src/slider.js
@@ -1,16 +1,22 @@
 const slides = document.getElementById("slides");
+let slideWidth = slides.children[0].clientWidth;
+const totalSlides = slides.children.length;
 
 let currentIndex = 0;
-const totalSlides = slides.children.length;
 
 const showHandler = (index) => {
   slides.style.transition = "transform 0.5s ease";
   slides.style.transform = `translateX(${-slideWidth * index}px)`;
 };
 
-// const showHandler = () => {
-//   showSlide(currentIndex);
-// };
+const slideImgs = [...document.getElementsByClassName("skill__Detail__img")];
+
+slideImgs.forEach((slideImg, index) => {
+  slideImg.onclick = () => {
+    currentIndex = index;
+    showHandler(index);
+  };
+});
 
 window.onresize = () => {
   slides.style.transition = "none";
