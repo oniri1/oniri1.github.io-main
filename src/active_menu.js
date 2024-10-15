7a50bea1c64440134376fbf12f55af2aff9e2638
oniri1 <oniri1@naver.com>
2024-10-15 14:08:43 +0900

    bugfix: Observer bug when catched 3 Sections

--- src/active_menu.js
+++ src/active_menu.js
@@ -1,9 +1,9 @@
 const sectionIDs = [
-  "#home",
-  "#skills__Details",
-  "#Worked",
-  "#summation",
-  "#Contact",
+  "#one_Section",
+  "#two_Section",
+  "#three_Section",
+  "#four_Section",
+  "#five_Section",
 ];
 const sections = sectionIDs.map((id) => document.querySelector(id));
 const navItems = sectionIDs.map((id) =>
@@ -28,15 +28,38 @@ function observercallback(entries) {
     const index = sectionIDs.indexOf(`#${entry.target.id}`);
 
     visibleSections[index] = entry.isIntersecting;
+
+    console.log("index", index);
+
     selectLastone =
       index === sectionIDs.length - 1 &&
       entry.isIntersecting &&
       entry.intersectionRatio >= 0.98;
+    console.log(
+      sectionIDs,
+      sectionIDs.length,
+      entry.isIntersecting,
+      entry.intersectionRatio
+    );
   });
 
-  const navIndex = selectLastone
-    ? sectionIDs.length - 1
-    : findFirstIntersecting(visibleSections);
+  console.log("셀렉트라스트", selectLastone);
+  console.log(visibleSections);
+
+  let navIndex = 0;
+
+  if (selectLastone) {
+    if (checkReallyLastIntersecting(visibleSections)) {
+      navIndex = visibleSections.indexOf(true) + 1;
+    } else {
+      navIndex = sectionIDs.length - 1;
+    }
+  } else {
+    navIndex = findFirstIntersecting(visibleSections);
+  }
+
+  checkReallyLastIntersecting(visibleSections);
+  console.log(navIndex);
 
   selectNavItem(navIndex);
 }
@@ -44,6 +67,18 @@ function observercallback(entries) {
 function findFirstIntersecting(intersections) {
   const index = intersections.indexOf(true);
   return index >= 0 ? index : 0;
+}
+
+function checkReallyLastIntersecting(intersections) {
+  console.log(intersections);
+  const indexs = intersections.filter((data) => {
+    return data === true;
+  });
+  if (indexs.length > 2) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 function selectNavItem(index) {
